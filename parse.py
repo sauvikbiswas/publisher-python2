de9b45d19c60d920d01001f578ea7c819411b4a1
sauvik <sauvik.biswas@gmail.com>
2017-04-27 19:55:59 +0530

    Some more parsing added

--- parse.py
+++ parse.py
@@ -2,14 +2,42 @@ from pprint import pprint as pp
 import re
 
 varDeclareRe = re.compile('^\s*\$\s*(\w+)\s*:\s*(.*)')
-# varDeclareRe = re.compile('^\s*\$')
+emptyLineRe = re.compile('^\s*$')
+varSubstituteRe = re.compile('\'\((\w+)\)')
 
 def parseVars(data):
+    modData = []
+    postVars = {}
+    flagFill = True
     for line in data:
         match = varDeclareRe.search(line)
         if match:
-            print match.group(1), ':', match.group(2)
-            flag = True
+            postVars[match.group(1)] = match.group(2)
+            flagFill = False
+        else:
+            if not flagFill:
+                match = emptyLineRe.search(line)
+                if not match:
+                    flagFill = True
+            if flagFill:
+                modData.append(line)
+    return modData, postVars
+
+def substituteVars(modData, postVars):
+    subData = []
+    for line in modData:
+        newLine = line
+        match = varSubstituteRe.findall(line)
+        replacementSet = set(match)
+        removalSet = set([])
+        for item in replacementSet:
+            if item not in postVars:
+                removalSet.add(item)
+        replacementSet = replacementSet - removalSet
+        for item in replacementSet:
+            newLine = newLine.replace('\'('+item+')', postVars[item])
+        subData.append(newLine)
+    return subData
 
 def readFile(filename):
     ''' Reads a markdown file and returns a list of strings '''
@@ -22,4 +50,5 @@ data = readFile('my_blog/header.md') + \
     readFile('my_blog/posts/2017-04-27-whoami.md')
 
 
-parseVars(data)
+modData, postVars = parseVars(data)
+pp(substituteVars(modData, postVars))
